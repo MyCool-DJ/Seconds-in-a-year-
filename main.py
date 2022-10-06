5353babecac85bb0542290b9f6e2f9c223b44b19
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-10-06 08:16:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#day 11 of 100 days of code
+daysInYear = int(input("How many days are in this year? "))
+hoursInDay = 24
+minutesInHour = 60
+secondsInMinute = 60
+
+secondsInYear = secondsInMinute * minutesInHour * hoursInDay * daysInYear
+
+print("There are ", secondsInYear, "seconds this year! Use them wisely 🙂")
